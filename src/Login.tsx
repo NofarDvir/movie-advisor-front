899f40f2d6cce917a56967d2112badbaf8d4b8bc
Nofar <nofardvir1@gmail.com>
2024-03-16 10:47:36 +0200

    Feat(login, register): add login and register pages

---
+++ src/Login.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+interface LoginProps {
+  onLogin: (email: string, password: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    onLogin(email, password);
+  };
+
+  const componentStyle = {
+    backgroundColor: "#e0e0e0",
+    padding: "20px",
+  };
+
+  return (
+    <div className="container mt-5" style={componentStyle}>
+      <center>
+        <h1>Login</h1>
+      </center>
+      <form className="row g-3 needs-validation">
+        <div className="mb-3">
+          <label htmlFor="validationTooltip01" className="form-label">
+            Email:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="validationTooltip01"
+            value={email}
+            placeholder="Email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="valid-tooltip">Looks good!</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="validationTooltip02" className="form-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            value={password}
+            id="validationTooltip02"
+            placeholder="Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="valid-tooltip">Looks good!</div>
+        </div>
+        <div className="d-grid gap-2 col-6 mx-auto">
+          <button
+            className="btn btn-primary"
+            type="submit"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </div>
+      </form>
+      <center>
+        <a href="#">Forgot password?</a>
+      </center>
+      <hr></hr>
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <button type="button" className="btn btn-primary" onClick={handleLogin}>
+          <img
+            src="./assets/google_logo.svg"
+            className="me-2"
+            style={{ width: "50px", height: "50px" }}
+          />
+          Sign in with Google
+        </button>
+      </div>
+      <center>
+        <a href="#">Don't have an account yet?</a>
+      </center>
+    </div>
+  );
+};
+
+export default Login;
